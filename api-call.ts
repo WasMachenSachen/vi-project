0037d2828745c155c03b1b3347d9cd2d85301557
Yannic Brügger <yannic@bruegger.dev>
2022-05-25 10:59:03 +0200

    add wait helper function

--- api-call.ts
+++ api-call.ts
@@ -1,5 +1,6 @@
 import "https://deno.land/x/dotenv/load.ts";
 import { writeJsonSync } from "https://deno.land/x/jsonfile/mod.ts";
+import { wait } from './helper.ts'
 
 const API_URL = Deno.env.get("API_URL");
 const API_KEY = Deno.env.get("API_KEY");
@@ -9,13 +10,13 @@ const responseArray: Record<string, unknown>[] = [];
 let cursor;
 
 for (let i = 0; i < MAX_REQUESTS; i++) {
-  const tempUrl = `${API_URL}?apikey=${API_KEY}${ cursor ? `&cursor=${cursor}` : '' }`;
-  const response = await fetch(tempUrl);
-  const jsonData = await response.json();
-
   console.log("durchgang:" + i);
   console.log({cursor});
 
+  const tempUrl = `${API_URL}?apikey=${API_KEY}${ cursor ? `&cursor=${cursor}` : '' }`;
+  const response = await fetch(tempUrl);
+  const jsonData = await response.json();
+  
   // deno-lint-ignore no-explicit-any
   jsonData?.documents?.forEach((document: any) => {
     const isDocumentRelevant = document?.vorgangsbezug.length > 0 && document.vorgangsbezug[0]?.vorgangsposition.includes("Ordnungsruf");
@@ -26,7 +27,9 @@ for (let i = 0; i < MAX_REQUESTS; i++) {
 
   if(!jsonData.cursor){break;}
   cursor = encodeURIComponent(jsonData.cursor);
+  await wait(10);
 }
+
 console.log("Gefundende Ordnungsrufe: " + responseArray.length);
 
 await Deno.mkdir("output", { recursive: true });
---
+++ helper.ts
@@ -0,0 +1,5 @@
+export function wait(seconds: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(true), 1000 * seconds);
+  });
+}
