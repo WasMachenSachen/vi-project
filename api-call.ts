d310d4c6a280373b424731167fbde8cb352777ba
Yannic Brügger <yannic@bruegger.dev>
2022-05-25 11:21:18 +0200

    make programm always end gracefully

--- api-call.ts
+++ api-call.ts
@@ -18,21 +18,29 @@ for (let i = 0; i < MAX_REQUESTS; i++) {
   console.log("durchgang:" + i);
   console.log({cursor});
   const tempUrl = `${API_URL}?apikey=${API_KEY}${ cursor ? `&cursor=${cursor}` : '' }`;
-  const response = await fetch(tempUrl);
-  const jsonData = await response.json();
   
-  // deno-lint-ignore no-explicit-any
-  jsonData?.documents?.forEach((document: any) => {
-    const isDocumentRelevant = document?.vorgangsbezug.length > 0 && document.vorgangsbezug[0]?.vorgangsposition.includes("Ordnungsruf");
-    if (isDocumentRelevant) {
-      responseArray.push(document);
-    }
-  });
+  try {
+    const response = await fetch(tempUrl);
+    const jsonData = await response.json();
 
-  if(cursor) cursors.push({i, cursor});
-  if(!jsonData.cursor){break;}
-  cursor = encodeURIComponent(jsonData.cursor);
-  await wait(TIMEOUT_IN_SECONDS);
+    
+    // deno-lint-ignore no-explicit-any
+    jsonData?.documents?.forEach((document: any) => {
+      const isDocumentRelevant = document?.vorgangsbezug.length > 0 && document.vorgangsbezug[0]?.vorgangsposition.includes("Ordnungsruf");
+      if (isDocumentRelevant) {
+        responseArray.push(document);
+      }
+    });
+  
+    if(cursor) cursors.push({i, cursor});
+    if(!jsonData.cursor){break;}
+    cursor = encodeURIComponent(jsonData.cursor);
+    await wait(TIMEOUT_IN_SECONDS);
+  } catch(e) {
+    console.error(e);
+    console.warn('Continue to quit gracefully.');
+    break;
+  }
 }
 
 console.log("Gefundende Ordnungsrufe: " + responseArray.length);
